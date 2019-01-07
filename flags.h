1f27d86af9c29439628df4d4e044ac606423d12d
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2019-01-07 16:15:48 +1000

    add some spaces

--- flags.h
+++ flags.h
@@ -7,10 +7,10 @@
 typedef uint8_t flag_t;
 #define FLAG_C(v) UINT8_C(v)
 
-#define FLAG_TEST_MASK(flags, mask, bits) ((bool)(((flag_t)(flags) & (flag_t)(mask)) == (flag_t)(bits)) )
-#define FLAG_TEST_ALL(flags, bits)        ((bool)(((flag_t)(flags) & (flag_t)(bits)) == (flag_t)(bits)) )
-#define FLAG_TEST_ANY(flags, bits)        ((bool)(((flag_t)(flags) & (flag_t)(bits)) != FLAG_C(0)) )
-#define FLAG_TEST_NONE(flags, bits)       ((bool)(((flag_t)(flags) & (flag_t)(bits)) == FLAG_C(0)) )
+#define FLAG_TEST_MASK(flags, mask, bits) ( (bool)(((flag_t)(flags) & (flag_t)(mask)) == (flag_t)(bits)) )
+#define FLAG_TEST_ALL(flags, bits)        ( (bool)(((flag_t)(flags) & (flag_t)(bits)) == (flag_t)(bits)) )
+#define FLAG_TEST_ANY(flags, bits)        ( (bool)(((flag_t)(flags) & (flag_t)(bits)) != FLAG_C(0)) )
+#define FLAG_TEST_NONE(flags, bits)       ( (bool)(((flag_t)(flags) & (flag_t)(bits)) == FLAG_C(0)) )
 
 #define FLAG_TEST(flags, bits) FLAG_TEST_ALL(flags, bits)
 
